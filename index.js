79ccb80210ad819f2f3faafdb28651eadd9884e9
tanmay982004 <155573005+tanmay982004@users.noreply.github.com>
2024-09-07 21:02:06 +0530

    github data added

--- index.js
+++ index.js
@@ -56,9 +56,9 @@ app.get('/youtube',(req,res)=>
     res.send('<h1>Hello Youtube</h1>')
 })
 app.get('/github',(req,res)=>
-{
-   res.json(githubData)
-})
+  {
+     res.json(githubData)
+  })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
